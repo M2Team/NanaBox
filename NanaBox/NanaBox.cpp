2d03805217f924cfee59275d3326551c448578f7
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-05-16 03:33:23 +0800

    Add NanaBox project.

---
+++ NanaBox/NanaBox.cpp
@@ -0,0 +1,31 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      NanaBox.cpp
+ * PURPOSE:   Implementation for NanaBox
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Windows.h>
+
+int WINAPI wWinMain(
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd)
+{
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(nShowCmd);
+
+    ::MessageBoxW(
+        nullptr,
+        L"Hello World!",
+        L"NanaBox",
+        0);
+
+    return 0;
+}
