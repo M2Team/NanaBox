dadccc2f7142584381534d5f501b90f0525e3a23
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-06-17 00:54:49 +0800

    Add Mile.Xaml project.

---
+++ Mile.Xaml/pch.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <Windows.h>
+
+// https://github.com/microsoft/terminal/blob
+// /c727762602b8bd12e4a3a769053204d7e92b81c5
+// /src/cascadia/WindowsTerminalUniversal/pch.h#L12
+// This is inexplicable, but for whatever reason, cppwinrt conflicts with the
+// SDK definition of this function, so the only fix is to undef it.
+// from WinBase.h
+// Windows::UI::Xaml::Media::Animation::IStoryboard::GetCurrentTime
+#ifdef GetCurrentTime
+#undef GetCurrentTime
+#endif
+
+#include <WinUser.h>
+
+#include <unknwn.h> // To enable support for non-WinRT interfaces, unknwn.h must be included before any C++/WinRT headers.
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Xaml.Hosting.h>
+#include <winrt/Windows.System.h>
