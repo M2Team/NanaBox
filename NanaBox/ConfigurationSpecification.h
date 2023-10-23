d275c24effd208ee2cce28a2153e2ff21854dd1a
MouriNaruto <KurikoMouri@outlook.jp>
2023-10-23 23:09:11 +0800

    Create Virtual Machine Configuration Specification header.

---
+++ NanaBox/ConfigurationSpecification.h
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      ConfigurationSpecification.h
+ * PURPOSE:   Definition for the Virtual Machine Configuration Specification
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANABOX_CONFIGURATION_SPECIFICATION
+#define NANABOX_CONFIGURATION_SPECIFICATION
+
+#endif // !NANABOX_CONFIGURATION_SPECIFICATION
