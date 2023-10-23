5e100b8fdb3ff3f4297d574d6d413be6ac459708
MouriNaruto <KurikoMouri@outlook.jp>
2023-10-23 23:23:47 +0800

    Create Virtual Machine Configuration Manager.

---
+++ NanaBox/ConfigurationManager.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      ConfigurationManager.h
+ * PURPOSE:   Implementation for the Virtual Machine Configuration Manager
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "ConfigurationManager.h"
---
+++ NanaBox/ConfigurationManager.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      ConfigurationManager.h
+ * PURPOSE:   Definition for the Virtual Machine Configuration Manager
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANABOX_CONFIGURATION_MANAGER
+#define NANABOX_CONFIGURATION_MANAGER
+
+#include "ConfigurationSpecification.h"
+
+namespace NanaBox
+{
+}
+
+#endif // !NANABOX_CONFIGURATION_MANAGER
