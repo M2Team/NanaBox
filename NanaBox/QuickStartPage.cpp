cbc4b76f1e7a92888eb5f32e21a945ac2a4cfebe
MouriNaruto <KurikoMouri@outlook.jp>
2023-11-19 07:29:05 +0800

    Start to implement the quick start window.

---
+++ NanaBox/QuickStartPage.cpp
@@ -0,0 +1,51 @@
+﻿#include "pch.h"
+#include "QuickStartPage.h"
+#if __has_include("QuickStartPage.g.cpp")
+#include "QuickStartPage.g.cpp"
+#endif
+
+#include "Utils.h"
+
+#include "NanaBoxResources.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::NanaBox::implementation
+{
+    QuickStartPage::QuickStartPage(
+        _In_ HWND WindowHandle) :
+        m_WindowHandle(WindowHandle)
+    {
+        ::SetWindowTextW(
+            this->m_WindowHandle,
+            Mile::WinRT::GetLocalizedString(
+                L"QuickStartPage/GridTitleTextBlock/Text").c_str());
+
+        HICON ApplicationIconHandle = reinterpret_cast<HICON>(::LoadImageW(
+            ::GetModuleHandleW(nullptr),
+            MAKEINTRESOURCE(IDI_NANABOX),
+            IMAGE_ICON,
+            256,
+            256,
+            LR_SHARED));
+        if (ApplicationIconHandle)
+        {
+            ::SendMessageW(
+                this->m_WindowHandle,
+                WM_SETICON,
+                TRUE,
+                reinterpret_cast<LPARAM>(ApplicationIconHandle));
+            ::SendMessageW(
+                this->m_WindowHandle,
+                WM_SETICON,
+                FALSE,
+                reinterpret_cast<LPARAM>(ApplicationIconHandle));
+        }
+    }
+
+    void QuickStartPage::InitializeComponent()
+    {
+        QuickStartPageT::InitializeComponent();
+    }
+}
---
+++ NanaBox/QuickStartPage.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "QuickStartPage.g.h"
+
+#include <Windows.h>
+
+namespace winrt::NanaBox::implementation
+{
+    struct QuickStartPage : QuickStartPageT<QuickStartPage>
+    {
+    public:
+
+        QuickStartPage(
+            _In_ HWND WindowHandle = nullptr);
+
+        void InitializeComponent();
+
+    private:
+
+        HWND m_WindowHandle;
+    };
+}
+
+namespace winrt::NanaBox::factory_implementation
+{
+    struct QuickStartPage :
+        QuickStartPageT<QuickStartPage, implementation::QuickStartPage>
+    {
+    };
+}
