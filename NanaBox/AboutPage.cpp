c065220e26bb9476f2fe4b49642cdc55d08a6b56
MouriNaruto <KurikoMouri@outlook.jp>
2023-11-19 07:50:17 +0800

    Implement the about window.

---
+++ NanaBox/AboutPage.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "AboutPage.h"
+#if __has_include("AboutPage.g.cpp")
+#include "AboutPage.g.cpp"
+#endif
+
+#include <Windows.h>
+
+#include <Mile.Project.Version.h>
+
+#include <winrt/Windows.UI.Xaml.Documents.h>
+
+#include "Utils.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::NanaBox::implementation
+{
+    AboutPage::AboutPage(
+        _In_ HWND WindowHandle) :
+        m_WindowHandle(WindowHandle)
+    {
+        ::SetWindowTextW(
+            this->m_WindowHandle,
+            Mile::WinRT::GetLocalizedString(
+                L"AboutPage/GridTitleTextBlock/Text").c_str());
+    }
+
+    void AboutPage::InitializeComponent()
+    {
+        AboutPageT::InitializeComponent();
+
+        this->Version().Text(winrt::hstring(
+            L"NanaBox " MILE_PROJECT_VERSION_STRING
+            L" (" MILE_PROJECT_DOT_VERSION_STRING L")"));
+    }
+
+    void AboutPage::GitHubButtonClick(
+        winrt::IInspectable const& sender,
+        winrt::RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        SHELLEXECUTEINFOW ExecInfo = { 0 };
+        ExecInfo.cbSize = sizeof(SHELLEXECUTEINFOW);
+        ExecInfo.lpVerb = L"open";
+        ExecInfo.lpFile = L"https://github.com/M2Team/NanaBox";
+        ExecInfo.nShow = SW_SHOWNORMAL;
+        ::ShellExecuteExW(&ExecInfo);
+    }
+
+    void AboutPage::CancelButtonClick(
+        winrt::IInspectable const& sender,
+        winrt::RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        ::DestroyWindow(this->m_WindowHandle);
+    }
+}
---
+++ NanaBox/AboutPage.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "AboutPage.g.h"
+
+#include <Windows.h>
+
+namespace winrt
+{
+    using Windows::Foundation::IInspectable;
+    using Windows::UI::Xaml::RoutedEventArgs;
+}
+
+namespace winrt::NanaBox::implementation
+{
+    struct AboutPage : AboutPageT<AboutPage>
+    {
+    public:
+
+        AboutPage(
+            _In_ HWND WindowHandle = nullptr);
+
+        void InitializeComponent();
+
+        void GitHubButtonClick(
+            winrt::IInspectable const& sender,
+            winrt::RoutedEventArgs const& e);
+
+        void CancelButtonClick(
+            winrt::IInspectable const& sender,
+            winrt::RoutedEventArgs const& e);
+
+    private:
+
+        HWND m_WindowHandle;
+    };
+}
+
+namespace winrt::NanaBox::factory_implementation
+{
+    struct AboutPage :
+        AboutPageT<AboutPage, implementation::AboutPage>
+    {
+    };
+}
