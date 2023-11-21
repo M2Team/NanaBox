7ac38af273361ce6727b8ee017a7ab4eb0e90644
MouriNaruto <KurikoMouri@outlook.jp>
2023-11-21 21:52:46 +0800

    Start to implement the reload virtual machine settings window.

---
+++ NanaBox/ReloadConfirmationPage.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "ReloadConfirmationPage.h"
+#if __has_include("ReloadConfirmationPage.g.cpp")
+#include "ReloadConfirmationPage.g.cpp"
+#endif
+
+#include "Utils.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::NanaBox::implementation
+{
+    ReloadConfirmationPage::ReloadConfirmationPage(
+        _In_ HWND WindowHandle) :
+        m_WindowHandle(WindowHandle)
+    {
+        ::SetWindowTextW(
+            this->m_WindowHandle,
+            Mile::WinRT::GetLocalizedString(
+                L"ReloadConfirmationPage/GridTitleTextBlock/Text").c_str());
+    }
+
+    void ReloadConfirmationPage::InitializeComponent()
+    {
+        ReloadConfirmationPageT::InitializeComponent();
+    }
+
+    void ReloadConfirmationPage::ReloadButtonClick(
+        winrt::IInspectable const& sender,
+        winrt::RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+    }
+
+    void ReloadConfirmationPage::CancelButtonClick(
+        winrt::IInspectable const& sender,
+        winrt::RoutedEventArgs const& e)
+    {
+        UNREFERENCED_PARAMETER(sender);
+        UNREFERENCED_PARAMETER(e);
+
+        ::DestroyWindow(this->m_WindowHandle);
+    }
+}
---
+++ NanaBox/ReloadConfirmationPage.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "ReloadConfirmationPage.g.h"
+
+#include <Windows.h>
+
+namespace winrt
+{
+    using Windows::Foundation::IInspectable;
+    using Windows::UI::Xaml::RoutedEventArgs;
+}
+
+namespace winrt::NanaBox::implementation
+{
+    struct ReloadConfirmationPage :
+        ReloadConfirmationPageT<ReloadConfirmationPage>
+    {
+    public:
+
+        ReloadConfirmationPage(
+            _In_ HWND WindowHandle = nullptr);
+
+        void InitializeComponent();
+
+        void ReloadButtonClick(
+            winrt::IInspectable const& sender,
+            winrt::RoutedEventArgs const& e);
+
+        void CancelButtonClick(
+            winrt::IInspectable const& sender,
+            winrt::RoutedEventArgs const& e);
+
+    private:
+
+        HWND m_WindowHandle;
+    };
+}
+
+namespace winrt::NanaBox::factory_implementation
+{
+    struct ReloadConfirmationPage : ReloadConfirmationPageT<
+        ReloadConfirmationPage, implementation::ReloadConfirmationPage>
+    {
+    };
+}
