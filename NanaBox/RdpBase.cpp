22e0db999a31451c95715238b865bee39c0e659a
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-12 10:37:43 +0800

    Create RdpBase.

---
+++ NanaBox/RdpBase.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      RdpBase.h
+ * PURPOSE:   Implementation for the Remote Desktop Base API Wrapper
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: MouriNaruto (KurikoMouri@outlook.jp)
+ */
+
+#include "RdpBase.h"
---
+++ NanaBox/RdpBase.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:   NanaBox
+ * FILE:      RdpBase.h
+ * PURPOSE:   Definition for the Remote Desktop Base API
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: MouriNaruto (KurikoMouri@outlook.jp)
+ */
+
+#ifndef RDPBASE_H
+#define RDPBASE_H
+
+#ifndef __cplusplus
+#error "[RdpBase] You should use a C++ compiler."
+#endif // !__cplusplus
+
+#include <comdef.h>
+
+#endif // !RDPBASE_H
