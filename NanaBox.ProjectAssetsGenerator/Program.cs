268e960f84a85b3c8d5235ca72143b64856477f2
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-20 01:44:33 +0800

    Add NanaBox.ProjectAssetsGenerator project.

---
+++ NanaBox.ProjectAssetsGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NanaBox.ProjectAssetsGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "NanaBox.ProjectAssetsGenerator task has been completed.");
+            Console.ReadKey();
+
+        }
+    }
+}
