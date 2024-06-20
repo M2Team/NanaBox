65d055dad9a9500a3253c6eac396ca86bcf72bff
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-20 22:50:13 +0800

    Create NanaBox.RefreshPackageVersion project.

---
+++ NanaBox.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NanaBox.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "NanaBox.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
