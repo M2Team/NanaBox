47422e35dd68d21cacaa80ca426201797bafeb04
MouriNaruto <Kenji.Mouri@outlook.com>
2024-06-20 23:11:08 +0800

    Implement RefreshAppxManifestVersion in NanaBox.RefreshPackageVersion.

--- NanaBox.RefreshPackageVersion/Program.cs
+++ NanaBox.RefreshPackageVersion/Program.cs
@@ -1,9 +1,58 @@
-﻿namespace NanaBox.RefreshPackageVersion
+﻿using Mile.Project.Helpers;
+using System.Xml;
+
+namespace NanaBox.RefreshPackageVersion
 {
     internal class Program
     {
+        static (int Major, int Minor) Version = (1, 3);
+        static string BuildStartDay = "2022-04-01";
+
+        static string GenerateVersionString()
+        {
+            return string.Format(
+                "{0}.{1}.{2}.0",
+                Version.Major,
+                Version.Minor,
+                DateTime.Today.Subtract(DateTime.Parse(BuildStartDay)).TotalDays);
+        }
+
+        static string RepositoryRoot = GitRepository.GetRootPath();
+
+        static void RefreshAppxManifestVersion()
+        {
+            string FilePath = string.Format(
+                @"{0}\NanaBoxPackage\Package.appxmanifest",
+                RepositoryRoot);
+
+            XmlDocument Document = new XmlDocument();
+            Document.PreserveWhitespace = true;
+            Document.Load(FilePath);
+
+            XmlNode? PackageNode = Document["Package"];
+            if (PackageNode != null)
+            {
+                XmlNode? IdentityNode = PackageNode["Identity"];
+                if (IdentityNode != null)
+                {
+                    XmlAttribute? VersionAttribute =
+                        IdentityNode.Attributes["Version"];
+                    if (VersionAttribute != null)
+                    {
+                        FileUtilities.SaveTextToFileAsUtf8Bom(
+                            FilePath,
+                            File.ReadAllText(FilePath).Replace(
+                                VersionAttribute.Value,
+                                GenerateVersionString()));
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            RefreshAppxManifestVersion();
+
             Console.WriteLine(
                 "NanaBox.RefreshPackageVersion task has been completed.");
             Console.ReadKey();
