8fadd5cd8594aadd6e80c361c967ae60a2ab0d64
MouriNaruto <Kenji.Mouri@outlook.com>
2024-11-19 23:25:22 +0800

    Create NanaBox.Configuration.Parser.

---
+++ NanaBox/NanaBox.Configuration.Parser.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      NanaBox.Configuration.Parser.cpp
+ * PURPOSE:   Implementation for Virtual Machine Configuration Parser
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "NanaBox.Configuration.Parser.h"
---
+++ NanaBox/NanaBox.Configuration.Parser.h
@@ -0,0 +1,21 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      NanaBox.Configuration.Parser.h
+ * PURPOSE:   Definition for Virtual Machine Configuration Parser
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANABOX_CONFIGURATION_PARSER
+#define NANABOX_CONFIGURATION_PARSER
+
+#include <NanaBox.Configuration.Specification.h>
+
+namespace NanaBox
+{
+
+}
+
+#endif // !NANABOX_CONFIGURATION_PARSER
