421e88fab37043805104867dceb2cefff8455d95
MouriNaruto <Kenji.Mouri@outlook.com>
2026-09-20 21:06:00 +0800

    Create VmmsCertificate.

---
+++ NanaBox/VmmsCertificate.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:    NanaBox
+ * FILE:       VmmsCertificate.h
+ * PURPOSE:    Implementation for the VMMS self-signed certificate management
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "VmmsCertificate.h"
---
+++ NanaBox/VmmsCertificate.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:    NanaBox
+ * FILE:       VmmsCertificate.h
+ * PURPOSE:    Definition for the VMMS self-signed certificate management
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANABOX_VMMSCERTIFICATE
+#define NANABOX_VMMSCERTIFICATE
+
+#ifndef __cplusplus
+#error "[VmmsCertificate] You should use a C++ compiler."
+#endif // !__cplusplus
+
+#include <Windows.h>
+
+#endif // !NANABOX_VMMSCERTIFICATE
